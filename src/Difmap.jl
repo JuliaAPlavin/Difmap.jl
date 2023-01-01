3f4b6ec060bbb5e3fce6befb687a5906e9a4a49b
aplavin <alexander@plav.in>
2023-01-01 00:00:00 +0000

    Deploying to gh-pages from  @ 56ee59757ad401186e1f7c61f089aa9e62e1111b 🚀

---
+++ src/Difmap.jl
@@ -0,0 +1,115 @@
+@doc let path = joinpath(dirname(@__DIR__), "README.md")
+    include_dependency(path)
+    replace(read(path, String), "```julia" => "```jldoctest mylabel")
+end module Difmap
+
+using DataPipes
+using difmap_jll
+using ImageMagick_jll: imagemagick_convert, identify
+
+
+Base.@kwdef struct ExecutionResult
+    exitcode::Int
+    success::Bool
+    stdout::String
+    stderr::String
+    log::Union{String, Nothing}
+    outfiles::Vector
+end
+
+Base.success(r::ExecutionResult) = r.success
+
+execute(script::Vector{String}; kwargs...) = execute(join(script, "\n"); kwargs...)
+
+function execute(script::String; in_files=[], out_files=[], out_files_overwrite=false)
+    original_dir = pwd()
+    mktempdir() do tmp_dir
+        @debug "Running difmap in $tmp_dir"
+        if !isempty(in_files)
+            @debug "Copying files $in_files from $original_dir to $tmp_dir"
+            for (from, to) in in_files
+                @assert !occursin("/", to) to
+                cp(joinpath(original_dir, from), joinpath(tmp_dir, to))
+            end
+        end
+        @assert !isfile(joinpath(tmp_dir, "difmap.log"))
+        @assert !isfile(joinpath(tmp_dir, "commands"))
+        open(joinpath(tmp_dir, "commands"), "w") do f
+            write(f, script)
+        end
+        
+        out = joinpath(tmp_dir, "__stdout")
+        err = joinpath(tmp_dir, "__stderr")
+        cmd = Cmd(`$(difmap())`, ignorestatus=true, dir=tmp_dir)
+        process = run(pipeline(cmd, stdin=joinpath(tmp_dir, "commands"), stdout=out, stderr=err), wait=true)
+        success = process.exitcode == 0
+
+        files = @p begin
+            readdir(tmp_dir)
+            filter(_ ∉ ["commands", "difmap.log"] && _ ∉ last.(in_files) && !startswith(_, "__"))
+            map() do f
+                if f ∈ first.(out_files)
+                    tgt = @p out_files |> filter(_[1] == f) |> only() |> __[2]
+                    isnothing(tgt) && return (name=f, path=nothing)
+                    tgt = joinpath(original_dir, tgt)
+                    cp(joinpath(tmp_dir, f), tgt, force=out_files_overwrite)
+                    (name=f, path=tgt)
+                else
+                    (name=f, path=nothing)
+                end
+            end
+        end
+        return ExecutionResult(
+            exitcode=process.exitcode,
+            success=success,
+            stdout=read(out, String),
+            stderr=read(err, String),
+            log=isfile(joinpath(tmp_dir, "difmap.log")) ? read(joinpath(tmp_dir, "difmap.log"), String) : nothing,
+            outfiles=files,
+        )
+    end
+end
+
+inputlines(res::ExecutionResult) = inputlines(res.log)
+inputlines(log::String) = @p split(log, "\n") |> filter(!isempty(_) && !startswith(_, "! "))
+outputlines(res::ExecutionResult) = outputlines(res.log)
+outputlines(log::String) = @p split(log, "\n") |> filter(startswith(_, "! ")) |> map(strip(_[2:end]))
+
+inout_pairs(res::ExecutionResult) = inout_pairs(res.log)
+function inout_pairs(log::String)
+    lines = @p split(log, "\n") |> map((line=_, kind=startswith(_, "! ") ? :out : :in))
+    result = ["" => String[]]
+    for s in lines
+        if s.kind == :in && !isempty(s.line)
+            push!(result, s.line => [])
+        elseif s.kind == :out
+            push!(result[end].second, strip(s.line[2:end]))
+        end
+    end
+    return result
+end
+
+struct Plot
+    file::String
+end
+
+function Base.show(io::IO, ::MIME"image/png", p::Plot)
+    open(p.file) do f
+        write(io, f)
+    end
+end
+
+function plots(res::ExecutionResult, args=`-density 100`)
+    @p res.outfiles |> filtermap(_.path) |> filtermap() do p
+        outfile = tempname()
+        identify() do exe
+            success(run(`$exe $p`; wait=false))
+        end || return nothing
+        imagemagick_convert() do exe
+            run(`$exe $args $(p) PNG:$(outfile)`)
+        end
+        Plot(outfile)
+    end
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,85 @@
+using Test
+using Difmap
+
+
+@testset "execute" begin
+    script = [
+        "print(1 + 2)",
+        "exit",
+    ]
+    res = Difmap.execute(script)
+    @test success(res)
+    @test isempty(res.outfiles)
+    @test res.stderr == "Exiting program\n"
+    @test Difmap.inputlines(res) == script
+    @test Difmap.outputlines(res) |> length == 4
+    @test occursin("Started logfile", Difmap.outputlines(res)[1])
+    @test Difmap.outputlines(res)[2] == "3"
+    @test occursin("Exiting program", Difmap.outputlines(res)[3])
+    @test occursin("difmap.log closed", Difmap.outputlines(res)[4])
+    @test Difmap.inout_pairs(res) |> length == length(script) + 1
+    @test Difmap.inout_pairs(res)[2] == ("print(1 + 2)" => ["3"])
+end
+
+@testset "copy file" begin
+    res = mktemp() do tempf, _
+        write(tempf, "\nprint \"2 * 2\"\n")
+        script = [
+            "print(1 + 2)",
+            "@my_script_file",
+            "exit",
+        ]
+        Difmap.execute(script, in_files=[tempf => "my_script_file"])
+    end
+    @test success(res)
+    @test isempty(res.outfiles)
+    @test res.stderr == "Exiting program\n"
+    @test Difmap.outputlines(res) |> length == 5
+    @test occursin("Started logfile", Difmap.outputlines(res)[1])
+    @test Difmap.outputlines(res)[2] == "3"
+    @test "2 * 2" == Difmap.outputlines(res)[3]
+    @test Difmap.inout_pairs(res) |> length == 6
+    @test Difmap.inout_pairs(res)[2] == ("print(1 + 2)" => ["3"])
+    @test Difmap.inout_pairs(res)[3] == ("![@my_script_file]" => [])
+    @test Difmap.inout_pairs(res)[4] == ("print \"2 * 2\"" => ["2 * 2"])
+end
+
+@testset "process vis data" begin
+    script = [
+        "observe $(joinpath(@__DIR__, "./data/vis.fits"))",
+        "select I",
+        "mapsize 512, 0.2",
+        "clean 500",
+        "restore",
+        "device tmp.ps/PS",
+        "mapplot cln",
+        "save result",
+        "exit",
+    ]
+    res = Difmap.execute(script)
+    @test success(res)
+    res = Difmap.execute(script, out_files=[["result.fits", "result.mod", "result.par", "result.uvf"] .=> nothing; "tmp.ps" => tempname()])
+    @test success(res)
+    @test map(f -> f[(:name,)], res.outfiles[[1, 2, 4]]) == [
+        (name = "result.fits",),
+        (name = "result.mod",),
+        (name = "result.uvf",),
+    ]
+    @test res.outfiles[end].name == "tmp.ps"
+    @test res.stderr == "Polarization I is unavailable.\nExiting program\n"
+    iops = Difmap.inout_pairs(res)
+    @test length(iops) == length(script) + 1
+    @test occursin(r"^AN table", iops[2].second[2])
+    @test occursin(r" J0000\+0248$", iops[2].second[4])
+    @test occursin(" 1152 visibilities", iops[2].second[end])
+    @test occursin(" = 512x512 pixels ", iops[4].second[end])
+
+    @test length(Difmap.plots(res)) == 1
+end
+
+
+using Documenter
+doctest(Difmap; manual=false)
+
+import CompatHelperLocal as CHL
+CHL.@check()
